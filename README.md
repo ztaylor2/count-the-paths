ca93c3d92e9028ecaf651529dde276694c5bd2c7
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-06 14:18:50 -0800

    setting up requirements, files, and env.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # count-the-paths
-Count the paths in an m by n matrix
+Count the paths in an m by n matrix.  
+
+Solutions include both recursive and dynamic.
---
+++ count_paths.py
@@ -0,0 +1,12 @@
+"""Path counting solutions."""
+
+def count_path_recursive(m, n):
+    """Count number of paths with the recursive method."""
+    pass
+
+
+def count_path_dynamic(m, n):
+    """Count number of paths with dynamic method."""
+    # create 2d array to store values
+    paths = [[0 for x in range(m)] for y in range(n)]
+    import pdb; pdb.set_trace()
