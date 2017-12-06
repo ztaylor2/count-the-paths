3d37b57c3c6eebb38e455bf4f55546e1281af7f7
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-06 15:13:38 -0800

    Built recursive method.

--- count_paths.py
+++ count_paths.py
@@ -1,8 +1,18 @@
 """Path counting solutions."""
 
+
 def count_path_recursive(m, n):
     """Count number of paths with the recursive method."""
-    pass
+    def traverse(m, n, location=[1, 1]):
+        # return 0 if past edge
+        if location[0] > m or location[1] > n:
+            return 0
+        # return 1 if at end position
+        if location == [m, n]:
+            return 1
+        return traverse(m, n, [location[0] + 1, location[1]]) + traverse(m, n, [location[0], location[1] + 1])
+
+    return traverse(m, n)
 
 
 def count_path_dynamic(m, n):
--- test_count_paths.py
+++ test_count_paths.py
@@ -1,8 +1,15 @@
 """Test path count algorithms."""
 
 
-def test_dynamic_two_d_array():
-    """Test the two d array of the dynamic solution."""
+def test_dynamic():
+    """Test dynamic method."""
     from count_paths import count_path_dynamic
-    count_three_by_three = count_path_dynamic(3, 3)
-    assert count_three_by_three == 6
+    count = count_path_dynamic(3, 3)
+    assert count == 6
+
+
+def test_recursive():
+    """Test recursive method."""
+    from count_paths import count_path_recursive
+    count = count_path_recursive(3, 3)
+    assert count == 6
